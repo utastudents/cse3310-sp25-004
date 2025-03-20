ad1d7e1f2da7d509cdd0ce7705892891255c4003
Bud Davis <jimmie.davis@uta.edu>
2025-03-20 08:53:32 -0500

    initial add of top level classes

---
+++ checkers/src/main/java/cse3310/uta/App.java
@@ -0,0 +1,13 @@
+package cse3310.uta;
+
+import cse3310.uta.PageManager.PageManager;
+
+public class App {
+    PageManager pm;
+
+    public static void main(String[] args) {
+        PageManager pm;
+        pm = new PageManager();
+        System.out.println("Hello World!");
+    }
+}
---
+++ checkers/src/main/java/cse3310/uta/Bot/BotII/BotII.java
@@ -0,0 +1,5 @@
+package cse3310.uta.Bot.BotII;
+
+public class BotII {
+    
+}
