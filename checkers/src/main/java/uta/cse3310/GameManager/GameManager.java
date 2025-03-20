feb40bd9751a674fb02764988ea908c85e862fe9
Bud Davis <jimmie.davis@uta.edu>
2025-03-20 10:45:41 -0500

    more details from top level design.

--- checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
@@ -1,5 +1,22 @@
 package uta.cse3310.GameManager;
 
+import uta.cse3310.GamePlay.GamePlay;
+import uta.cse3310.GameTermination.GameTermination;
+import uta.cse3310.Bot.BotI.BotI;
+import uta.cse3310.Bot.BotII.BotII;
+
 public class GameManager {
-    
+    GamePlay gp;
+    GameTermination gt;
+    BotI b1;
+    BotII b2;
+
+    public GameManager() {
+        gp = new GamePlay();
+        gt = new GameTermination();
+        b1 = new BotI();
+        b2 = new BotII();
+
+    }
+
 }
