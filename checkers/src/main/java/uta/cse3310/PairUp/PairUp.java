761f82904c93fffaac093215a30aacd7e37b3747
Sam Mach <webmaster@smach.us>
2025-03-26 15:52:55 -0500

    Adds bot challenges

--- checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
@@ -3,6 +3,7 @@ package uta.cse3310.PairUp;
 import java.util.Queue;
 
 import uta.cse3310.DB.DB;
+import uta.cse3310.Bot.*;
 
 public class PairUp {
     private Queue<Challenge> playerQueue;
@@ -13,12 +14,20 @@ public class PairUp {
 
     public boolean addToQueue(Player p) {return false;} //Adds a player to the Queue
     public boolean challenge(Player p, Player c) {return false;} //Makes 2 players challenge eachother (or player & bot, or 2 bots)
+    public boolean challengeBot(Player p, boolean botI) {
+        //return challenge(p, botI ? new BotI() : new BotII()); //just calls challenge with a bot
+        return false;
+    }
+    public boolean botVBot(boolean botI, boolean botII) {
+        //return challenge(botI ? new BotI() : new BotII(), botI ? new BotI() : new BotII()); //just calls challenge with a bot
+        return false;
+    }
 
     public boolean boardAvailable() {return false;} //Called by GameManager when a board becomes available (in case all were previously taken)
 
 }
 
-//Private class for handling challegnge queue
+//Private class for handling challenge queue
 class Challenge {
     Player first;
     Player second;
