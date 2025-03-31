d0f102ee6150018fd9c260d468ed63cfa58a155d
Sam Mach <webmaster@smach.us>
2025-03-31 15:19:16 -0500

    Makes Bot an extension of Player

--- checkers/src/main/java/uta/cse3310/Bot/Bot.java
+++ checkers/src/main/java/uta/cse3310/Bot/Bot.java
@@ -1,5 +1,9 @@
 package uta.cse3310.Bot;
 
-public abstract class Bot {
+import uta.cse3310.PairUp.Player;
+
+
+//Bot I and Bot II should both extend this class
+public abstract class Bot extends Player {
     
 }
