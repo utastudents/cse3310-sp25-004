f8b07ef39ca5cd582a58bc5be07f8173d0ac52d5
pierevans <pier_evans@yahoo.com>
2025-03-31 15:42:07 -0500

    updating public db function

--- checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
@@ -1,15 +1,16 @@
 package uta.cse3310.PairUp;
 
 import java.util.Queue;
-
+import java.util.LinkedList;
 import uta.cse3310.DB.DB;
 import uta.cse3310.Bot.*;
 
 public class PairUp {
     private Queue<Challenge> playerQueue;
-
+    private DB db;
     public PairUp(DB db) {
-        
+                this.playerQueue = new LinkedList<>();
+                this.db = db;
     }
 
     public boolean addToQueue(Player p) {return false;} //Adds a player to the Queue
