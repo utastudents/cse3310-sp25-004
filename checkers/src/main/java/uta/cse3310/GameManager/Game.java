f8b297974594ffb650151ac7217a0192a98e4bb5
ija1333 <ija1333@mavs.uta.edu>
2025-04-01 14:58:06 -0500

    roughdraft pt 2

--- checkers/src/main/java/uta/cse3310/GameManager/Game.java
+++ checkers/src/main/java/uta/cse3310/GameManager/Game.java
@@ -1,27 +1,36 @@
 package uta.cse3310.GameManager;
 
 //import uta.cse3310.PairUp.PairUp;
-//import uta.cse3310.PairUp.Player;                for compiling purpose 
-//import uta.cse3310.GamePlay.GamePlay;
+//import uta.cse3310.PairUp.Player;              //  for compiling purpose 
+import uta.cse3310.GamePlay.GamePlay;
 
 public class Game{
 
-   //private Player player1;       // work on making sure pair up data matches up 
-   //private Player player2;
-   //private GamePlay gameplay;     decide later how to use when gameplay is more fleshed out 
+   //private int player1;       // work on making sure pair up data matches up 
+  // private int player2;
+   private GamePlay board;     //decide later how to use when gameplay is more fleshed out 
    //private boolean isGameActive;
    //public int gameID
 
 
-	//Roughdraft for controlling the game 
+	//Roughdraft for controlling the game board wise 
+
+
 
 		
-	public Game(int gameID, Player player1, Player player2){
+	public Game(int gameID, int playerId){ // might need to handle challenge Q in here too 
+     // i think need pair up needs to make a function to that returns player ID so we can call and store it for whatever we need 
+	
+   
+   }
 
-	}
 
-	public void checkGameStatus() {
+   public GamePlay getBoard(){
+      return board;
+   }
+
+   //get board might interact with intergeres being x,y dont know exactly how its going to work 
+	
         
-    	}
-
+          
 }
