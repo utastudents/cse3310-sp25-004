637eb8cd5e9607e153795097d8543099783e4810
mikaelamrmb <mdm7008@mavs.uta.edu>
2025-04-01 13:49:14 -0500

    GameID addition

--- checkers/src/main/java/uta/cse3310/GameManager/Game.java
+++ checkers/src/main/java/uta/cse3310/GameManager/Game.java
@@ -6,34 +6,22 @@ package uta.cse3310.GameManager;
 
 public class Game{
 
-	//private Player player1;       // work on making sure pair up data matches up 
-   // private Player player2;
-   // private GamePlay gameplay;     decide later how to use when gameplay is more fleshed out 
-   // private boolean isGameActive;
+   //private Player player1;       // work on making sure pair up data matches up 
+   //private Player player2;
+   //private GamePlay gameplay;     decide later how to use when gameplay is more fleshed out 
+   //private boolean isGameActive;
+   //public int gameID
 
 
 	//Roughdraft for controlling the game 
 
 		
-	public Game(){
-
-	}
-
-	public void startGame(){
-
-	}
-
-	public void makeMove(){
+	public Game(int gameID, Player player1, Player player2){
 
 	}
 
 	public void checkGameStatus() {
         
-    }
-
-	public void endGame() {
-        
-    }
-
+    	}
 
 }
