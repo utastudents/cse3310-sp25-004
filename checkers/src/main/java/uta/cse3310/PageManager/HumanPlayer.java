83596c1be5db870e2d6df83c854ed4d6e5d00177
“Omar <ona6594@mavs.uta.edu>
2025-04-01 16:29:22 -0500

    Created general layout of the class, waiting for the fields it has

--- checkers/src/main/java/uta/cse3310/PageManager/HumanPlayer.java
+++ checkers/src/main/java/uta/cse3310/PageManager/HumanPlayer.java
@@ -2,3 +2,25 @@ package uta.cse3310.PageManager;
 
 import uta.cse3310.PairUp;
 
+public class HumanPlayer extends Player{
+    // constructor
+    // public HumanPlayer()
+    // {
+
+    // }
+
+
+    // These methods will be implemented soon
+
+    @Override
+    public boolean makeMove(/** BoardState */); //Returns false if the client could not be reached
+    {
+
+    }
+
+    @Override
+    public boolean updateBoard(/** BoardState */) //Returns false if the client could not be reached
+    {
+
+    }
+}
