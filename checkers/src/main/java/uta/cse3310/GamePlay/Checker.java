c60044cc62867634b8d95cd01d5faa2bb7d48a33
marioluigijam <mullinsj881@gmail.com>
2025-04-01 17:45:49 -0500

    Added Checker Class
    Added Checker class with its constructor

---
+++ checkers/src/main/java/uta/cse3310/GamePlay/Checker.java
@@ -0,0 +1,15 @@
+package uta.cse3310.GamePlay;
+
+/*
+ * Class for checker object
+ */
+public class Checker {
+	boolean isKing = false;
+	Cord cord;
+	Color color;
+	
+	public Checker(Cord cord, Color color){
+		this.cord = cord;
+		this.color = color;
+	}
+}
