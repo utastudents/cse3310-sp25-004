c2b43b98ee3a9ddcb464a60ef3d24bbe93761653
dominikC0824 <ddc6216@mavs.uta.edu>
2025-04-01 22:18:15 -0500

    Update BotII.java
    Added 3 methods

--- checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
@@ -4,4 +4,11 @@ public class BotII extends Bot {
     public void makeValidMove() {
         // TODO: Logic to choose and make a legal move
     }
+    public void promoteToKing() {
+        // TODO: Check if a piece reached the end and promote to king / Allow more than one king on board
+    }
+    public void defendPieces() {
+        // TODO: Prioritize defending own pieces over offense
+        // TODO: Try to block opponent from advancing or jumping
+    }
 }
