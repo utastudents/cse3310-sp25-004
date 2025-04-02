4a5dcc64ab9924f5da1818b4b188ad1d1d4f526b
wbs8341 <wbs8341@mavs.uta.edu>
2025-04-02 15:41:52 -0500

    Update DB.java
    added public class and defined the linked list, that and my name/user ID

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -1,5 +1,15 @@
+
+/* William Sarratt 1001858341 */
 package uta.cse3310.DB;
 
+import java.util.LinkedList; 
+import uta.cse3310.PageManager.HumanPlayer;
+        
 public class DB {
-
-}
+    private LinkedList<HumanPlayer> players;
+    private int nextPlayerId = 1;
+         
+    public DB() {
+        players = new LinkedList<>();
+    }
+  
