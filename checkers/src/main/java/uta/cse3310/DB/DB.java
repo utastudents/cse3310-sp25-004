9ce493a947cec8ffccdb9527c4a2f0bb3d89a3cd
wbs8341 <wbs8341@mavs.uta.edu>
2025-04-02 15:43:00 -0500

    Update DB.java

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -5,11 +5,13 @@ package uta.cse3310.DB;
 import java.util.LinkedList; 
 import uta.cse3310.PageManager.HumanPlayer;
         
-public class DB {
+public class DB 
+{
     private LinkedList<HumanPlayer> players;
     private int nextPlayerId = 1;
          
-    public DB() {
+    public DB() 
+        {
         players = new LinkedList<>();
     }
-  
+}
