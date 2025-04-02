187c43da36b89aa8843a9c7db670217658106569
mikaela <mdm7008@mavs.uta.edu>
2025-04-02 16:02:57 -0500

    Removal of Pair Up Instnace

--- checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
@@ -1,27 +1,25 @@
 package uta.cse3310.GameManager;
 
-import uta.cse3310.GamePlay.GamePlay;
+// import uta.cse3310.GamePlay.GamePlay;
 import uta.cse3310.GameTermination.GameTermination;
 import uta.cse3310.Bot.BotI.BotI;
 import uta.cse3310.Bot.BotII.BotII;
-import uta.cse3310.PairUp.PairUp;
+import uta.cse3310.GameManager.Game;
 import java.util.ArrayList;
 
 public class GameManager {
-    GamePlay gp;
+    // GamePlay gp;
     GameTermination gt;
     BotI b1;
     BotII b2;
-    PairUp pu;
     private ArrayList<Game> games = new ArrayList<>();
-    private int maxGames = 10; // essentially a global variable we can check later on
+    private static final int maxGames = 10; // essentially a global variable we can check later on
    
     public GameManager() {
-        gp = new GamePlay();
+        // gp = new GamePlay();
         gt = new GameTermination();
         b1 = new BotI();
         b2 = new BotII();
-        pu = new PairUp();
         
         games = new ArrayList<>(maxGames);
    }
