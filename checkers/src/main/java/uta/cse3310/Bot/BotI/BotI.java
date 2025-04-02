568a0ae16df66ac927e3db83fd5bd6e2bda0396a
NixonThong <nxt8383@mavs.uta.edu>
2025-04-02 15:41:43 -0500

    Update BotI.java

--- checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
@@ -1,76 +1,65 @@
 package uta.cse3310.Bot.BotI;
 
 public class BotI extends Bot {
-    // req 1
+
     public void makeValidMove() {
     }
-    // req 2
+    
     public void promoteToKing() {
     }
-    // req 3
+    
     public void defendPieces() {
     }
-    // req 4
+    
     public void capturePieces() {
     }
-    // req 5
+    
      public void checkMultipleJumps() {
         // TODO: If multiple jumps are possible, do them
     }
 
-    // Req 6
     public void moveChecker() {
         // TODO: Move a non-king piece diagonally one square
     }
 
-    // Req 7
     public void moveKing() {
         // TODO: Move a king piece diagonally one square in any direction
     }
 
-    // Req 8
     public boolean isMoveLegal() {
         // TODO: Validate if a move is legal
         return true;
     }
-
-    // Req 9
+    
     public void waitForOpponent() {
         // TODO: Wait for opponent to make a move before acting
     }
 
-    // Req 10
     public void adjustStrategy() {
         // TODO: Change strategy based on early, mid, or late game
     }
 
-    // Req 11
     public void blockOpponent() {
         // TODO: Try to block opponent from advancing or jumping
     }
 
-    // Req 12
     public void findOffensiveMove() {
         // TODO: Decide if it's safe and smart to attack
     }
 
-    // Req 13
     public void allowMultipleKings() {
         // TODO: Allow more than one king on board
     }
 
-    // Req 14
     public boolean isPieceCaptured(int pieceId) {
         // TODO: Check if a piece has been captured
         return false;
     }
 
-    // Req 15
     public void checkMultipleCaptures() {
         // TODO: Detect and respond if multiple pieces were taken
     }
 
-    // Req 16
     public void protectBackLine() {
         // TODO: Avoid moving back row pieces unless necessary
     }
