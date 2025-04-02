69029a54526c58aba9a9bd9fcc6500d9bdc262ff
Sam Mach <webmaster@smach.us>
2025-04-02 14:00:49 -0500

    Adds queue spectators

--- checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
@@ -1,17 +1,19 @@
 package uta.cse3310.PairUp;
 
 import java.util.Queue;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import uta.cse3310.DB.DB;
 import uta.cse3310.Bot.BotI.BotI;
 import uta.cse3310.Bot.BotII.BotII;
+import uta.cse3310.PageManager.HumanPlayer;
 
 public class PairUp {
     private Queue<Challenge> playerQueue;
     private DB db;
     public PairUp(DB db) {
-                this.playerQueue = new LinkedList<>();
-                this.db = db;
+        this.playerQueue = new LinkedList<>();
+        this.db = db;
     }
 
     public boolean addToQueue(Player p) {return false;} //Adds a player to the Queue
@@ -20,7 +22,7 @@ public class PairUp {
         //return challenge(p, botI ? new BotI() : new BotII()); //just calls challenge with a bot
         return false;
     }
-    public boolean botVBot(boolean botI, boolean botII) {
+    public boolean botVBot(boolean botI, boolean botII, HumanPlayer spectator) {
         //return challenge(botI ? new BotI() : new BotII(), botI ? new BotI() : new BotII()); //just calls challenge with a bot
         return false;
     }
@@ -33,7 +35,9 @@ public class PairUp {
 class Challenge {
     Player first;
     Player second;
+    ArrayList<HumanPlayer> spectators;
     boolean hasJustOne;
+
     public Challenge(Player p) {
         first = p;
         hasJustOne = true;
