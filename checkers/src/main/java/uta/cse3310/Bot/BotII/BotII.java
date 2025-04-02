00f0ae3cbc5722832a354f0a0b71e82615566339
dominikC0824 <ddc6216@mavs.uta.edu>
2025-04-01 21:55:06 -0500

    Update BotII.java

--- checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
@@ -1,5 +1,7 @@
 package uta.cse3310.Bot.BotII;
 
-public class BotII extends Bot{
-    
+public class BotII extends Bot {
+    public void makeValidMove() {
+        // TODO: Logic to choose and make a legal move
+    }
 }
