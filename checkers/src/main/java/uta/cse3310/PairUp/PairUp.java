0049b16f6a84ea78550515ac059d3eebd5a4781d
uta-sxm7324 <sxm7324@mavs.uta.edu>
2025-04-02 22:52:33 -0500

    Adds javadocs to PairUp class

--- checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
@@ -12,6 +12,10 @@ public class PairUp {
     private DB db;
     private int numPlayersInQueue;
 
+    /**
+     * Create a new PairUp object. Should only be called ONCE, and only by Page Manager.
+     * @param db
+     */
     public PairUp(DB db) {
         this.playerQueue = new LinkedList<>();//Queue of players waiting to be paired
         this.db = db; //Database object for storing player data
@@ -22,19 +26,63 @@ public class PairUp {
     private boolean isInRange(Player p1, Player p2) {return true;} //Compares elo scores. If either is not a HumanPlayer, return true
     private void pairUp() {} //This is where the actual pairing will take place. Will be called by boardAvailable, challenge, and addToQueue
 
-    public boolean addToQueue(Player p) {return false;} //Adds a player to the Queue
-    public boolean challenge(Player p, Player c) {return false;} //Makes 2 players challenge eachother (or player & bot, or 2 bots)
+    /**
+     * Add a player to the challenge queue. Called by PageManager when a client requests matchmaking
+     * @param p - the Player who requested the matchmaking
+     * @return
+     */
+    public boolean addToQueue(Player p) {return false;}
+
+    /**
+     * Called by PageManager when a user requests a challenge, but it has not been accepted yet.
+     * The request should be forwarded to the second user.
+     * @param p - The player who requested the challenge
+     * @param c - The player who is being challenged.
+     * @return
+     */
+    public boolean requestChallenge(Player p, Player c) {return false;}
+
+    /**
+     * Add a player v player challenge to the queue - after it has been accepted
+     * @param p - the player who requested the challenge
+     * @param c - the player who accepted the challenge
+     * @return
+     */
+    public boolean challenge(Player p, Player c) {return false;}
+    /**
+     * Add a player v bot challenge to the queue
+     * @param p - The player who requested the challenge
+     * @param botI - the bot (True for bot I, false for bot II)
+     * @return
+     */
     public boolean challengeBot(Player p, boolean botI) {
         //return challenge(p, botI ? new BotI() : new BotII()); //just calls challenge with a bot
         return false;
     }
+    /**
+     * Add a bot v bot challenge to the queue
+     * @param botI - True for bot I, false for bot II
+     * @param botII - True for bot I, false for bot II
+     * @param spectator - the player who requested to watch
+     * @return
+     */
     public boolean botVBot(boolean botI, boolean botII, HumanPlayer spectator) {
         //return challenge(botI ? new BotI() : new BotII(), botI ? new BotI() : new BotII()); //just calls challenge with a bot
         return false;
     }
 
-    public boolean boardAvailable() {return false;} //Called by GameManager when a board becomes available (in case all were previously taken)
+    /**
+     * Event method, to let PairUp know that a board is now available that previously was not.
+     * Called by GameManager when a game is ended and the board is clear.
+     * @return
+     */
+    public boolean boardAvailable() {return false;}
 
+    /**
+     * Returns the number of players in the queue.
+     * For Join Game page to display to users.
+     * @return
+     */
     public int getNumPlayersInQueue() {return numPlayersInQueue;}
 }
 
