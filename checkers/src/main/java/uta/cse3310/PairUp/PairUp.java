19e7debe67ce2b30957f4506d4e7714b4453fb2c
pierevans <pier_evans@yahoo.com>
2025-04-02 21:58:11 -0500

    fixed

--- checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
@@ -11,18 +11,11 @@ import uta.cse3310.PageManager.HumanPlayer;
 public class PairUp {
     private Queue<Challenge> playerQueue;
     private DB db;
-<<<<<<< HEAD
-
-    public PairUp(DB db) {
-        this.playerQueue = new LinkedList<>(); //Queue of players waiting to be paired
-        this.db = db; //Database object for storing player data
-=======
     private int numPlayersInQueue;
     public PairUp(DB db) {
         this.playerQueue = new LinkedList<>();
         this.db = db;
         numPlayersInQueue = 0;
->>>>>>> 9e2eea4ea65a5c95c4c7a3a90ac3e305b20ceb74
     }
 
     private boolean isInRange(Player p1, Player p2) {return true;} //Compares elo scores. If either is not a HumanPlayer, return true
