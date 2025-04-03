ce9ccd695f027c2ebc173d955225dc7e4d764401
nxm3647 <nxm3647@mavs.uta.edu>
2025-04-03 11:40:49 -0500

    Update DB.java

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -7,11 +7,43 @@ import uta.cse3310.PageManager.HumanPlayer;
         
 public class DB 
 {
-    private LinkedList<HumanPlayer> players;
-    private int nextPlayerId = 1;
+        private LinkedList<HumanPlayer> players;
+        private int nextPlayerId = 1;
          
-    public DB() 
+        public DB() 
         {
-        players = new LinkedList<>();
-    }
+                players = new LinkedList<>();
+        }
+        /* Nandisha Maharjan */
+        /* Insert a new player to the DB */
+        public void addPlayer(String username, String password) 
+        {
+                HumanPlayer newPlayer = new HumanPlayer(username, password);
+                players.add(newPlayer);
+        }
+                
+        /* Get player by playerId */
+        public HumanPlayer getPlayerById(int playerId) 
+        {
+                for (HumanPlayer player : players) 
+                {
+                        if (player.getPlayerId() == playerId) 
+                        {
+                                return player;
+                        }
+                }
+                return null;  /* Player not found */
+        }
+                
+        /* Get player by username */
+        public HumanPlayer getPlayerByUsername(String username) 
+        {
+                for (HumanPlayer player : players) 
+                {
+                        if (player.getUsername().equals(username)) 
+                        {
+                                return player;
+                        }
+                }
+                return null;  /* Player not found */
 }
