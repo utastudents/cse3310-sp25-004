e13430299015274047c3929b201695c29781a1bd
Bryan G. <123043512+bxg1624@users.noreply.github.com>
2025-04-03 01:30:00 -0500

    Update GameManager.java
    accidentally committed changes changed it back to how it previously was. vs code mistake, sorry.

--- checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
@@ -10,16 +10,10 @@ import java.util.ArrayList;
 
 public class GameManager {
     // GamePlay gp;
-<<<<<<< HEAD
-    private static final int MAX_GAMES = 10;// essentially a global variable we can check later on
-    BotI b1;
-    BotII b2;
-=======
     private static final int MAX_GAMES = 10;// essentially a global variable we can change later on
     BotI b1;
     BotII b2;
     GameTermination gt;
->>>>>>> 54ad28769fd10d579bfe9c7d55da29b9c5e7fd66
     private ArrayList<Game> games = new ArrayList<>(MAX_GAMES);
     private ArrayList<Integer> numOfGames = new ArrayList<>();
 
@@ -32,52 +26,6 @@ public class GameManager {
         b2 = new BotII();
         
         games = new ArrayList<>(MAX_GAMES);
-<<<<<<< HEAD
-   }
-    // Initialize first 10 games
-   public void initializeGames() {	// most likely a for loop over ArrayList and adding games with Game Play board and players
-    for (int i = 0; i < MAX_GAMES; i++) {
-        games.add(new Game());
-        numOfGames.add(i); // marking these as available
-    }
-}
-  //Track numOfGames[] ArrayList for available game slots
-public ArrayList<Integer> getNumOfGames() {
-    ArrayList<Integer> availableSlots = new ArrayList<>();
-    for (int i = 0; i < games.size(); i++) {
-        if (games.get(i) == null) {
-            availableSlots.add(i);
-        }
-    }
-    return availableSlots;
-}
-
-   
-
-    // Create a new game from Pair Up
-   public boolean createGame(Player one, Player two, ArrayList<Player> spectator){
-    return boardAvailable(one,two,spectator);
-   	// if checkAvailableGames returns true, call pu.boardAvailable(); to start a new game
-   }
-   
-   public void removeGame(){
-   	// not sure if needed (?) have to check with GameTermination if they're removing game fully or we do
-   }
-   //Check for null in ArrayList Games
-
-   public boolean checkAvailableGames(){
-    for (Game game : games) {
-        if (game == null) {
-            return true;
-        }
-    }
-    
-   	// code implementing later to by checking if elements in ArrayList is null then set return value to true, else false
-   	return false;
-   }
-   
-}
-=======
     }
 
     // Initialize first 10 games
@@ -121,4 +69,3 @@ public ArrayList<Integer> getNumOfGames() {
         return false;
     }   
 }
->>>>>>> 54ad28769fd10d579bfe9c7d55da29b9c5e7fd66
