d577b7c4ece9c7be93bac08270dc5eab2141854f
Bryan G. <123043512+bxg1624@users.noreply.github.com>
2025-04-03 01:33:35 -0500

    Update gameState.java

--- checkers/src/main/java/uta/cse3310/GameTermination/gameState.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/gameState.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package uta.cse3310.GameTermination;
 
 import java.util.HashMap;
@@ -28,11 +27,3 @@ public class gameState{
     
     
 }
-
-
-=======
-package uta.cse3310.GameTermination
-
-import uta.cse3310.GameManager.Game
->>>>>>> 54ad28769fd10d579bfe9c7d55da29b9c5e7fd66
-
