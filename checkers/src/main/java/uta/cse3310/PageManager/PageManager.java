e23d0b4463a8c6dbc1a7ede53ede3c179583e80a
“Omar <ona6594@mavs.uta.edu>
2025-04-02 19:53:59 -0500

    made a switch to allow specific PageManager methods to execute

--- checkers/src/main/java/uta/cse3310/PageManager/PageManager.java
+++ checkers/src/main/java/uta/cse3310/PageManager/PageManager.java
@@ -7,33 +7,16 @@ import uta.cse3310.PairUp.PairUp;
 import uta.cse3310.PageManager.UserEvent;
 import uta.cse3310.PageManager.UserEventReply;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class PageManager {
     DB db;
     PairUp pu;
     Integer turn = 0; // just here for a demo. note it is a global, effectively and
                       // is not unique per client (or game)
-
-    public UserEventReply ProcessInput(UserEvent U) {
-        UserEventReply ret = new UserEventReply();
-        ret.status = new game_status();
-        // fake data for the example
-        if (turn == 0) {
-            ret.status.turn = 1;
-            turn = 1;
-        } else {
-            ret.status.turn = 0;
-            turn = 0;
-        }
-
-        // for now, the idea is to send it back where it came from
-        // in the future, all of the id's that need the data will need to
-        // be added to this list
-        ret.recipients = new ArrayList<>();
-        ret.recipients.add(U.id);
-
-        return ret;
-
-    }
 
     public PageManager() {
         db = new DB();
@@ -60,7 +43,37 @@ public class PageManager {
     // 
     
     
-    
+    public UserEventReply getActivePlayers(JsonObject jsonObj, int Id)
+    {
+        return null;
+    }
+
+    public UserEventReply joinQueue(JsonObject jsonObj, int Id)
+    {
+        return null;
+    }
+
+    public UserEventReply challengePlayer(JsonObject jsonObj, int Id)
+    {
+        return null;
+    }
+
+    public UserEventReply challengeBot(JsonObject jsonObj, int Id)
+    {
+        return null;
+    }
+
+    public UserEventReply BotvsBot(JsonObject jsonObj, int Id)
+    {
+        return null;
+    }
+
+    public UserEventReply ViewMatch(JsonObject jsonObj, int Id)
+    {
+        return null;
+    }
+
+
 
 
 
