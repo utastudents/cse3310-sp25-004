57b81a55cdde3417241499c33a8e05bf84fe5105
uta-sxm7324 <sxm7324@mavs.uta.edu>
2025-04-03 09:57:41 -0500

    Adds return documentation

--- checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
+++ checkers/src/main/java/uta/cse3310/PairUp/PairUp.java
@@ -30,7 +30,7 @@ public class PairUp {
     /**
      * Add a player to the challenge queue. Called by PageManager when a client requests matchmaking
      * @param p - the Player who requested the matchmaking
-     * @return
+     * @return - false if the player was not added to the queue, true otherwise
      */
     public boolean addToQueue(Player p) {return false;}
 
@@ -39,7 +39,7 @@ public class PairUp {
      * The request should be forwarded to the second user.
      * @param p - The player who requested the challenge
      * @param c - The player who is being challenged.
-     * @return
+     * @return - false if the player was not notified, true otherwise
      */
     public boolean requestChallenge(Player p, Player c) {return false;}
 
@@ -47,14 +47,14 @@ public class PairUp {
      * Add a player v player challenge to the queue - after it has been accepted
      * @param p - the player who requested the challenge
      * @param c - the player who accepted the challenge
-     * @return
+     * @return - false if the challengers were not added to the queue, true otherwise
      */
     public boolean challenge(Player p, Player c) {return false;}
     /**
      * Add a player v bot challenge to the queue
      * @param p - The player who requested the challenge
      * @param botI - the bot (True for bot I, false for bot II)
-     * @return
+     * @return - false if the challengers were not added to the queue, true otherwise
      */
     public boolean challengeBot(Player p, boolean botI) {
         //return challenge(p, botI ? new BotI() : new BotII()); //just calls challenge with a bot
@@ -65,7 +65,7 @@ public class PairUp {
      * @param botI - True for bot I, false for bot II
      * @param botII - True for bot I, false for bot II
      * @param spectator - the player who requested to watch
-     * @return
+     * @return - false if the challengers were not added to the queue, true otherwise
      */
     public boolean botVBot(boolean botI, boolean botII, HumanPlayer spectator) {
         //return challenge(botI ? new BotI() : new BotII(), botI ? new BotI() : new BotII()); //just calls challenge with a bot
@@ -75,14 +75,14 @@ public class PairUp {
     /**
      * Event method, to let PairUp know that a board is now available that previously was not.
      * Called by GameManager when a game is ended and the board is clear.
-     * @return
+     * @return - false if there was an error, true otherwise
      */
     public boolean boardAvailable() {return false;}
 
     /**
      * Returns the number of players in the queue.
      * For Join Game page to display to users.
-     * @return
+     * @return - number of players in the queue
      */
     public int getNumPlayersInQueue() {return numPlayersInQueue;}
 }
