aa4d26a2f4328f8dc7dbd852b2ce63eed17f76c8
Tomas Menchaca <atm7812@mavs.uta.edu>
2025-04-12 20:48:39 -0500

    implemented new fucntions in gameManager

--- checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
@@ -11,7 +11,6 @@ import uta.cse3310.PageManager.GameMove;
 
 import java.util.ArrayList;
 
-
 public class GameManager {
     // GamePlay gp;
     private static final int MAX_GAMES = 10;// essentially a global variable we can change later on
@@ -21,26 +20,24 @@ public class GameManager {
     private ArrayList<Game> games = new ArrayList<>(MAX_GAMES);
     private ArrayList<Integer> numOfGames = new ArrayList<>();
 
-
-   
     public GameManager() {
         // gp = new GamePlay();
         gt = new GameTermination();
         b1 = new BotI();
         b2 = new BotII();
-        
+
         games = new ArrayList<>(MAX_GAMES);
     }
 
     // Initialize first 10 games
     public void initializeGames() {
         for (int i = 0; i < MAX_GAMES; i++) {
-            games.add(new Game(0,-1));
+            games.add(new Game(0, -1));
             numOfGames.add(i); // marking these as available
         }
     }
-    
-    //Track numOfGames[] ArrayList for available game slots
+
+    // Track numOfGames[] ArrayList for available game slots
     public ArrayList<Integer> getNumOfGames() {
         ArrayList<Integer> availableSlots = new ArrayList<>();
         for (int i = 0; i < games.size(); i++) {
@@ -51,39 +48,55 @@ public class GameManager {
         return availableSlots;
     }
 
-    
     // Create a new game from Pair Up
-    public boolean createGame(Player p1, Player p2){
+    public boolean createGame(Player p1, Player p2) {
         ArrayList<Integer> availableSlots = getNumOfGames();
         if (!availableSlots.isEmpty()) {
             int index = availableSlots.get(0);
             Game newGame = new Game(index, 1); // Game ID = index, state = 1 (example)
-            /* newGame.setPlayer1(p1);
-            newGame.setPlayer2(p2); */
+            /*
+             * newGame.setPlayer1(p1);
+             * newGame.setPlayer2(p2);
+             */
             games.set(index, newGame);
             System.out.println("Game created at index: " + index);
             return true;
-         }
+        }
         System.out.println("No available game slots.");
         return false;
         // return boardAvailable(p1, p2, spectator); (?)
     }
 
-    
-    public void removeGame(){
+    public void removeGame() {
         Game gameToRemove = gt.endGame();
-        for (int i = 0; i < games.size(); i++){
+        for (int i = 0; i < games.size(); i++) {
             Game g = games.get(i);
-            if(g != null && g.getGameID() == gameToRemove.getGameID())
-            {
+            if (g != null && g.getGameID() == gameToRemove.getGameID()) {
                 games.set(i, null);
             }
         }
-        // if GameTermination sends a Game object that should end, check Game ID and remove here
-    } 
+        // if GameTermination sends a Game object that should end, check Game ID and
+        // remove here
+    }
 
-    /*public GameUpdate processMove(GameMove move){
-        // call GamePlay Board method to validate move ? and return GameUpdate object with new position and player ID
-        return new GameUpdate();
-    }*/
+    /*
+     * public GameUpdate processMove(GameMove move){
+     * // call GamePlay Board method to validate move ? and return GameUpdate object
+     * with new position and player ID
+     * 
+     * // Making sure validation is called and if not then return -1
+     * if(!moveValidation(move)){
+     * 
+     * return null;
+     * 
+     * }
+     * // Making new gameUpdate object
+     * updateGameResult newStats = new updateGameResult(move);
+     * 
+     * return newStats;
+     * 
+     * 
+     * }
+     */
+
 }
