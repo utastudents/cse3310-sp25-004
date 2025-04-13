7f89c234fc65835d6d61d6f339ae4cf429562e84
Bryan Gamboa <bryangamboa@MacBookPro.attlocal.net>
2025-04-13 16:56:20 -0500

    added check for all possible legal moves

--- checkers/src/main/java/uta/cse3310/GameTermination/gameState.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/gameState.java
@@ -7,25 +7,44 @@ import uta.cse3310.GamePlay.Cord;
 
 public class gameState{
 
-//function to see if player can still make legal moves; will use same
-//function as gamePlay.
+
+//function to see if a player can still make legal moves
 public boolean canPlayerMove(Board board, int playerID){
 
-//assume player has moves
-//replace logic to check board for valid moves
-for (int y = 0; y < 8; y++) {
-    for (int x = 0; x < 8; x++) {
-        Checker piece = board.checkerBoard[y][x];
-        if (piece != null && piece.getPlayerID() == playerID) {
-// Check for possible moves or jumps
-if (!board.getPossibleForwardJump(piece).isEmpty() ||
-!board.getPossibleBackwardJump(piece).isEmpty()) {
-    return true;
+    //assume player has moves
+    //replace logic to check board for valid moves
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            Checker piece = board.checkerBoard[y][x];
+    
+            if (piece != null && piece.getPlayerID() == playerID) {
+                // Check for possible moves or jumps
+                if (!board.getPossibleForwardJump(piece).isEmpty() ||
+                    !board.getPossibleBackwardJump(piece).isEmpty()){
+                        return true;
+                    }
+                //checks non-jump diagonal moves
+                int dir = (piece.getColor().toString().equals("BLACK")) ? 1 : -1;
+                //checks forward left    
+                if(isValidNormalMove(board, x - 1, y + dir)) return true;
+                //checks forward right        
+                if(isValidNormalMove(board, x + 1, y + dir)) return true;
+                //check king piece
+                if(piece.isKing()){
+                    //checks backward left
+                    if(isValidNormalMove(board, x - 1, y - dir)) return true;
+                    //checks backward right
+                    if(isValidNormalMove(board, x + 1, y - dir)) return true;
+                }
+            }    
+        }        
+    }
+    
+    return false;
 }
-}
-}
-}
-return false;
+
+private boolean isValidNormalMove(Board board, int x, int y){
+    return x >= 0 && x < 8 && y >= 0 && y < 8 && board.checkerBoard[y][x] == null;
 }
 
 //function to see if player has won
