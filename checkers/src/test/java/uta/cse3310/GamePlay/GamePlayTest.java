6d554ed1ac9fa5937a78e925f5cc15f45dc04130
michellePhan06 <mtp7872@mavs.uta.edu>
2025-04-15 13:05:27 -0500

    remove spaces in group 7 interface file name

--- checkers/src/test/java/uta/cse3310/GamePlay/GamePlayTest.java
+++ checkers/src/test/java/uta/cse3310/GamePlay/GamePlayTest.java
@@ -6,15 +6,17 @@ public class GamePlayTest{
 
 	@Test
 	void moveForwardCheckTest() {
-		// var checker = new Checker(true, new Cord(4, 1), BLACK);
+		// var checker = new Checker(new Cord(4, 1), BLACK);
 		// var cord = new Cord(5, 2);
 		// var board = new Board();
 		
 		// assertEquals(true, board.moveForwardCheck(checker, cord));
 	}
 
+    // testing piece will become a king piece once it reaches the end
     @Test
     void kingMeTest() {
+        // var checker = new Checker
 
     }
 
