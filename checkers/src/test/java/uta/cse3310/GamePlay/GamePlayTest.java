640c69c7bcc5ace4a3573c445ee1ad1119359bc6
michellePhan06 <mtp7872@mavs.uta.edu>
2025-04-15 11:41:01 -0500

    Add Game Play Test Folder

---
+++ checkers/src/test/java/uta/cse3310/GamePlay/GamePlayTest.java
@@ -0,0 +1,17 @@
+// package uta.cse3310.GamePlay;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import org.junit.jupiter.api.Test;
+
+// class Tests {
+
+// 	@Test
+// 	void moveForwardCheckTest() {
+// 		var checker = new Checker(true, new Cord(4, 1), BLACK);
+// 		var cord = new Cord(5, 2);
+// 		var board = new Board();
+		
+// 		assertEquals(true, board.moveForwardCheck(checker, cord));
+// 	}
+
+// }
