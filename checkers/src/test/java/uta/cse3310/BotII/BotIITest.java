32077db85b2a11f68b2b934e68270622614e9a72
Tatenda Chireza <tatendachireza@Tatendas-Air.dhost.uta.edu>
2025-04-16 15:32:35 -0500

    implement testGetSafeMoves()

--- checkers/src/test/java/uta/cse3310/BotII/BotIITest.java
+++ checkers/src/test/java/uta/cse3310/BotII/BotIITest.java
@@ -1,6 +1,8 @@
 package uta.cse3310.BotII;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import uta.cse3310.Bot.BotII.BotII;
@@ -39,6 +41,39 @@ public class BotIITest {
     @Test
     void capturePieces() {
 
+    }
+
+    @Test
+    public void testDefendPieces() {
+        // Set up the board with a black checker
+        Board board = new Board();
+        Checker blackChecker = new Checker(new Cord(3, 3), Color.BLACK);
+        board.checkerBoard[3][3] = blackChecker;
+
+        // Simulate a red piece in a position that can jump the black piece
+        Checker redChecker = new Checker(new Cord(4, 4), Color.RED);
+        board.checkerBoard[4][4] = redChecker;
+
+        // Call the defendPieces method to find a safe move
+        BotII.Move bestMove = BotII.defendPieces(board);
+
+        // Check if the bot finds a valid move
+        assertTrue("A defensive move should be found", bestMove != null);
+    }
+
+
+    @Test
+    public void testGetSafeMoves() {
+        // Set up the board and a black checker
+        Board board = new Board();
+        Checker blackChecker = new Checker(new Cord(3, 3), Color.BLACK);
+        board.checkerBoard[3][3] = blackChecker;
+
+        // Get the safe moves for the black checker
+        ArrayList<Cord> safeMoves = BotII.getSafeMoves(blackChecker, board);
+
+        // Assert that there are safe moves available
+        assertTrue("There should be at least one safe move", !safeMoves.isEmpty());
     }
     
 }
