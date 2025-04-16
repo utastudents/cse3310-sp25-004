d0350dff5e5b79c7792da8304a56c36772489af2
devw45 <devin.wingfield45@gmail.com>
2025-04-16 15:42:32 -0500

    fixed merge conflicts

--- checkers/src/main/webapp/socket.js
+++ checkers/src/main/webapp/socket.js
@@ -15,7 +15,6 @@ connection = new WebSocket(serverUrl);
 
 // Messenger - This is how ALL outgoing messages will be sent to the server. ALL OF THEM. 
 function sendMessage(json = {}) {
-<<<<<<< HEAD
     jsonMsg = JSON.parse(json);
 
     if (!jsonMsg.action) {
@@ -26,16 +25,6 @@ function sendMessage(json = {}) {
     console.log("Sending message " + jsonMsg.action + " to server");
     console.log(JSON.stringify(jsonMsg));
     connection.send(jsonMsg);
-=======
-    if (!json.action) {
-        console.trace("No ACTION specified in sendMessage! Msg:");
-        console.log(json);
-        return;
-    }
-    console.log("Sending message " + json.action + " to server");
-    console.log(JSON.stringify(json));
-    connection.send(JSON.stringify(json));
->>>>>>> main
 }
 
 
@@ -54,15 +43,12 @@ connection.onmessage = function (evt) {
     console.log("Message received: " + msg);
     let jsonMsg = JSON.parse(msg);
 
-<<<<<<< HEAD
-=======
     console.log(jsonMsg);
 
     let responseID = jsonMsg.responseID;
 
     console.log("Response ID: " + responseID);
 
->>>>>>> main
     if (jsonMsg.clientId) {
         globalClientID = jsonMsg.clientId;
         console.log("Connected with clientId:", globalClientID);
@@ -94,11 +80,7 @@ connection.onmessage = function (evt) {
     //convert data to JSON
     //Extracts the response identifier from the response, so we can determine whose code it belongs to
     //See ./INTERFACES/client-server-docs.md for details
-<<<<<<< HEAD
-    let responseID = jsonMsg.responseID;
-=======
     
->>>>>>> main
     switch (responseID )
     {
         // Page Manager
@@ -136,12 +118,10 @@ connection.onmessage = function (evt) {
         }
 
         // Game Responses
-<<<<<<< HEAD
         case "getActivePlayers": {
             updateJoinGameList(jsonMsg);
             break;
         }
-=======
         case "startGame": {
             //Start the actual game!
             //{"responseID":"startGame","gameType":"pvb","player1":{"isBot":false,"playerClientId":1,"username":"test","elo":0,"gamesWon":0,"gamesLost":0,"status":"IN_GAME"},"player2":{"isBot":true}}
@@ -149,7 +129,6 @@ connection.onmessage = function (evt) {
             break;
         }
 
->>>>>>> main
         // Summary Responses
         case "summaryData": {
             console.log("Received summary data for leaderboard!");
@@ -168,32 +147,14 @@ connection.onmessage = function (evt) {
     }
 }
 
-<<<<<<< HEAD
-// document.getElementById("join_game").style.display = "none"; // set Login to visible but the rest hidden
-// document.getElementById("game_display").style.display = "none";
-// document.getElementById("new_account").style.display = "none";
-// document.getElementById("login").style.display = "block"; 
-=======
-/*document.getElementById("join_game").style.display = "none"; // set Login to visible but the rest hidden
-document.getElementById("game_display").style.display = "none";
-document.getElementById("new_account").style.display = "none";
-document.getElementById("login").style.display = "block"; */
->>>>>>> main
 
-// function handleJoinGame(data) { //function to update when join team
-//     console.log("Join team response received", data);
 
-<<<<<<< HEAD
-//     document.getElementById("join_game").style.display = "block"; // set join game to visible and the rest to hidden
-//     document.getElementById("game_display").style.display = "none";
-//     document.getElementById("new_account").style.display = "none";
-//     document.getElementById("login").style.display = "block"; 
-// }
-=======
+
+function handleJoinGame(data) { //function to update when join team
+    console.log("Join team response received", data);
     document.getElementById("join_game").style.display = "block"; // set join game to visible and the rest to hidden
     document.getElementById("game_display").style.display = "none";
     document.getElementById("new_account").style.display = "none";
     document.getElementById("login").style.display = "none"; 
 }
->>>>>>> main
 
