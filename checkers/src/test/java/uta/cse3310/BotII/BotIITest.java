1d4031e99d1f937fc2624b847bc4aa2467f4db8f
Tatenda Chireza <tatendachireza@Tatendas-Air.lan>
2025-04-15 20:10:08 -0500

    Bot 2 unit test file

---
+++ checkers/src/test/java/uta/cse3310/BotII/BotIITest.java
@@ -0,0 +1,24 @@
+package uta.cse3310.BotII;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class BotIITest {
+
+    @Test
+	void Makemove() {
+		
+	}
+
+    @Test
+    void defendPieces() {
+        
+
+    }
+
+    @Test
+    void capturePieces() {
+
+    }
+    
+}
