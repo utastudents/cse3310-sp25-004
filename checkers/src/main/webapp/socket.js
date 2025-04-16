fcb9fc8c520fd079b0a98b21415c3eefcce7e751
Life Live On <tnkchaseme@gmail.com>
2025-04-15 22:33:20 -0500

    Logined players should see their name in the waiting list

--- checkers/src/main/webapp/socket.js
+++ checkers/src/main/webapp/socket.js
@@ -86,8 +86,16 @@ connection.onmessage = function (evt) {
         }
         case "loginSuccessful": {
             loginSuccess();
+            sendMessage({action:"getActivePlayers"});
             break;
         }
+
+        case "join_game":{
+            console.log("Join Game Received: ", jsonMsg);
+            updateJoinGameList(jsonMsg);
+            break;
+        }
+
         case "new_user": {
             newUser(jsonMsg.msg);
             break;
