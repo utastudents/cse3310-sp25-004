5f62b494abd5760c3195f687125fb2dee786d7e2
mikaela <mdm7008@mavs.uta.edu>
2025-04-16 16:54:45 -0500

    Removed getter/ setter tests and fixed MockPlayer

--- checkers/src/test/java/uta/cse3310/GameManager/GameTest.java
+++ checkers/src/test/java/uta/cse3310/GameManager/GameTest.java
@@ -30,67 +30,9 @@ public class GameTest {
         }
 
         @Override
-        public boolean endGame(GameState gs) {
+        public boolean endGame(GameState gs){
             return true;
         }
-    }
-
-    /**
-     * Testing that the constructor correctly sets the game ID and player1 ID.
-     */
-    @Test
-    void testConstructorAndGetters() {
-        int expectedGameID = 123;
-        Player player1 = new MockPlayer(42);
-        Player player2 = new MockPlayer(77);
-
-        // Creating a new Game object with the specified IDs
-        Game game = new Game(expectedGameID, player1, player2);
-
-        // Verifying that the game ID and player1 ID were correctly assigned
-        assertEquals(expectedGameID, game.getGameID(),
-                "Game ID should match the value set in the constructor");
-
-        assertEquals(player1, game.getPlayer1(),
-                "Player 1 ID should match the value set in the constructor");
-    }
-
-    /**
-     * Tests that setPlayer2() correctly sets and retrieves player2 ID.
-     */
-    @Test
-    void testSetPlayer2() {
-
-        Player player1 = new MockPlayer(10);
-        Player player2 = new MockPlayer(20);
-
-        Game game = new Game(200, player1, null); // Creating a Game with dummy IDs
-
-        // Setting player 2 using the setter method
-        game.setPlayer2(player2);
-
-        // Verifying that the player2 ID was set correctly
-        assertEquals(player2, game.getPlayer2(),
-                "Player 2 ID should match the value set by setPlayer2()");
-    }
-
-    /**
-     * Tests that setPlayer1() correctly updates and retrieves the player1 ID.
-     */
-    @Test
-    void testSetPlayer1() {
-
-        Player original = new MockPlayer(1);
-        Player newPlayer = new MockPlayer(77);
-
-        Game game = new Game(300, original, null); // Initial ID doesn't matter here
-
-        // Set a new player1 ID
-        game.setPlayer1(newPlayer);
-
-        // Check if the new player1 ID is correctly updated
-        assertEquals(newPlayer, game.getPlayer1(),
-                "Player 1 ID should match the value set by setPlayer1()");
     }
 
     @Test
