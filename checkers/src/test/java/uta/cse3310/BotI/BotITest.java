66775b445ff8cca10d3db1aa2e24bb98f05e1638
marcosadeluna <mad5661@mavs.uta.edu>
2025-04-17 13:31:26 -0500

    updated test files

---
+++ checkers/src/test/java/uta/cse3310/BotI/BotITest.java
@@ -0,0 +1,18 @@
+package uta.cse3310.BotI;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import uta.cse3310.Bot.BotI.BotI;
+
+
+public class BotITest {
+    //@Test
+
+    //@Test
+
+    //@Test
+
+}
