2f34c4a5f97da308e750615b16766e9d24b391e7
uta-axp4358 <axp4358@mavs.uta.edu>
2025-04-17 15:47:20 -0500

    Update BotI.java

--- checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
@@ -89,18 +89,116 @@ public class BotI extends Bot {
  
     private boolean wouldBecomeKing(Move move) {}
 
-    private boolean capturesKing(Move move) {} 
+    private boolean capturesKing(Move move) 
+    {
+     // If board is null, return false 
+    if (board == null) 
+    { 
+     return false; 
+    }  
+    int capturedX = (move.piece.getCord().getX() + move.destination.getX()) / 2; 
+    int capturedY = (move.piece.getCord().getY() + move.destination.getY()) / 2; 
+    Checker capturedPiece = board.checkSpace(new Cord(capturedX, capturedY)); 
+    return capturedPiece != null && capturedPiece.isKing(); 
+    } 
 
-    private boolean isSafeMove(Move move) {} 
-
-    private boolean isAdvancingMove(Move move) {} 
-
-    private void executeMove(Move move) {} 
-
-    private class Move {} 
-
-
-    */
+    private boolean isSafeMove(Move move) 
+    { 
+        if (board == null)
+        { 
+        return true; 
+        } 
+        for (int dy = -1; dy <= 1; dy += 2)
+        { 
+            for (int dx = -1; dx <= 1; dx += 2)
+            { 
+                int x = move.destination.getX() + dx; 
+                int y = move.destination.getY() + dy; 
+                if (x >= 0 && x < 8 && y >= 0 && y < 8)
+                { 
+                Checker adjacentPiece = board.checkSpace(new Cord(x, y)); 
+                if (adjacentPiece != null && adjacentPiece.getColor() != color)
+                { 
+                if (adjacentPiece.isKing() ||  
+                (adjacentPiece.getColor() == Color.BLACK && y < move.destination.getY()) || 
+                (adjacentPiece.getColor() == Color.RED && y > move.destination.getY())) 
+                { 
+                    int behindX = move.destination.getX() - dx; 
+                    int behindY = move.destination.getY() - dy; 
+                    if (behindX >= 0 && behindX < 8 && behindY >= 0 && behindY < 8)
+                    { 
+                        Checker behindPiece = board.checkSpace(new Cord(behindX, behindY)); 
+                        if (behindPiece != null && behindPiece.getColor() == color) 
+                        { 
+                            return true; 
+                        } 
+                    } 
+                    return false; 
+                } 
+                } 
+                } 
+            } 
+        } 
+            return true; 
+        }  
+private boolean isAdvancingMove(Move move)
+{ 
+    if (move.piece.isKing()) 
+    { 
+        return false; // Kings can move in any direction 
+    } 
+    if (move.piece.getColor() == Color.BLACK)
+    { 
+        return move.destination.getY() > move.piece.getCord().getY(); 
+    } 
+    else
+    { 
+        return move.destination.getY() < move.piece.getCord().getY(); 
+    } 
+}  
+private void executeMove(Move move)
+{ 
+    if (board != null)
+    { 
+        board.updatePosition(move.piece, move.destination); 
+        board.kingMe(move.piece); 
+    } 
+    } 
+    private class Move 
+    { 
+        Checker piece; 
+        Cord destination; 
+        boolean isJump; 
+        Move(Checker piece, Cord destination, boolean isJump)
+        { 
+            this.piece = piece; 
+            this.destination = destination; 
+            this.isJump = isJump; 
+        } 
+    } 
+} 
+private void executeMove(Move move)
+{ 
+    if (board != null)
+    { 
+        board.updatePosition(move.piece, move.destination); 
+        board.kingMe(move.piece); 
+    } 
+} 
+private class Move
+{ 
+    Checker piece; 
+    Cord destination; 
+    boolean isJump; 
+    Move(Checker piece, Cord destination, boolean isJump)
+    { 
+        this.piece = piece; 
+        this.destination = destination; 
+        this.isJump = isJump; 
+    } 
+} 
+} 
+*/
 
 }
 
