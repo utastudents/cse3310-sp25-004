e5cf9c2259fbc2fc618fd1ff42eaa78aeb7db323
serahjolly <sgj4801@mavs.uta.edu>
2025-04-17 14:14:55 -0500

    Update BotITest.java
    bot1 headers and functions emthods

--- checkers/src/test/java/uta/cse3310/BotI/BotITest.java
+++ checkers/src/test/java/uta/cse3310/BotI/BotITest.java
@@ -1,18 +1,47 @@
 /*package uta.cse3310.BotI;
-import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-
+import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import uta.cse3310.Bot.BotI.BotI;
+import uta.cse3310.GameManager.Game;
+import uta.cse3310.GamePlay.Board;
+import uta.cse3310.GamePlay.Checker;
+import uta.cse3310.GamePlay.Color;
+import uta.cse3310.GamePlay.Cord;
+import uta.cse3310.GamePlay.GamePlay;
+import uta.cse3310.GameState;
 
+public class BotITestI {
+    private BotI bot;
+    private Game game;
+    private Board board;
+    private GamePlay gameplay;
+    
+    @Before
+    public void setUp() {
+        gameplay = new GamePlay();
+        board = gameplay.getBoard();
+        game = new Game(1, null, null);
+        game.startGame(game);
 
-public class BotITest {
+        bot = new BotI(game, Color.BLACK);
+    }
+    
     //@Test
+    public void testMakeMove() {
+
+    }
 
     //@Test
+    public void testUpdateBoard() {
+
+    }
 
     //@Test
+    public void testEndGame() {
+
+    }
 
 }*/
