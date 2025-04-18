9907bd1fb22bf02ad4f96aabf5b75853478d2eb6
serahjolly <sgj4801@mavs.uta.edu>
2025-04-17 23:15:31 -0500

    clean compile changes

--- checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
@@ -1,14 +1,22 @@
-package uta.cse3310.Bot.BotI;
+package uta.cse3310.Bot.BotI; 
 
-import java.util.Random;
 
+import uta.cse3310.GameState; 
 import uta.cse3310.Bot.Bot;
-import uta.cse3310.GameManager.Game;
-import uta.cse3310.GamePlay.Board;
+import uta.cse3310.Bot.BotII.BotII.Move;
+import uta.cse3310.GameManager.Game; 
+import uta.cse3310.GamePlay.Board; 
+import uta.cse3310.GamePlay.Checker;
 import uta.cse3310.GamePlay.Color;
-import uta.cse3310.GameState;
+import uta.cse3310.GamePlay.Cord; 
+import uta.cse3310.GamePlay.GamePlay; 
+import uta.cse3310.PairUp.Player; 
+import java.util.ArrayList; 
+import java.util.Random; 
+import java.util.List; 
 
-public class BotI extends Bot {    
+public class BotI extends Bot {
+    
     private Board board;
     private Color color;
     private Game game;
@@ -27,11 +35,10 @@ public class BotI extends Bot {
         this.random = new Random();
     }
     
-    public boolean makeMove(GameState gs) {
-        // Ensure this method matches the abstract method signature in Bot
+    @Override
+    public boolean makeMove(GameState gs){
         
-        /* DOES NOT COMPILE - FIX AND TEST BEFORE COMITTING
-        ArrayList<Checker> availableCheckers = getAvailableCheckers();
+        ArrayList<Checker> availableCheckers = getAvailableCheckers(); 
         if (availableCheckers.isEmpty()) { 
             return false; 
         } 
@@ -49,42 +56,39 @@ public class BotI extends Bot {
             Move bestMove = selectBestMove(moves); 
             executeMove(bestMove); 
             return true; 
-        } */
-
+        } 
         return false; 
     } 
+       
+
 
 
     @Override 
     public boolean updateBoard(GameState gs) {
-        /*
+        
         if (game != null) {
             this.board = game.getBoard().getBoard();
         }
         return true;
-        */
-        return false;
+        
     } 
 
     @Override 
     public boolean endGame(GameState gs) {
-        /*
+        
         if (game != null) {
             this.board = game.getBoard().getBoard();
         }
         return true;
-        */
-        return false;
+        
     } 
+    
 
-    // Get all checkers of the bot's color 
-    /*
+    
 
     protected ArrayList<Checker> getAvailableCheckers() { 
         
         ArrayList<Checker> checkers = new ArrayList<>(); 
-        
-        // If board is null, return empty list 
         if (board == null) { 
             return checkers; 
         } 
@@ -100,34 +104,24 @@ public class BotI extends Bot {
         return checkers; 
     }  
 
-    // Get all possible jump moves for all checkers 
+    
 
     private ArrayList<Move> getAllJumpMoves(ArrayList<Checker> checkers) { 
 
         ArrayList<Move> jumpMoves = new ArrayList<>(); 
-
-        // If board is null, return empty list 
-
         if (board == null) { 
-
             return jumpMoves; 
         } 
 
         for (Checker checker : checkers) { 
-
             ArrayList<Cord> jumps; 
 
             if (checker.getColor() == Color.BLACK) { 
-
                 jumps = board.getPossibleForwardJump(checker); 
-
             } else { 
-
                 jumps = board.getPossibleBackwardJump(checker); 
             } 
-
             for (Cord jump : jumps) { 
-
                 jumpMoves.add(new Move(checker, jump, true)); 
             } 
         } 
@@ -135,169 +129,119 @@ public class BotI extends Bot {
         return jumpMoves; 
     } 
 
-   // Get all possible moves for all checkers
+   
     private ArrayList<Move> getAllMoves(ArrayList<Checker> checkers){
-	
-	ArrayList<Move> moves = new ArrayList<>(); 
-        
-	// If board is null, return empty list 
+	    ArrayList<Move> moves = new ArrayList<>(); 
 
         if (board == null) { 
-
             return moves; 
         } 
 
         for (Checker checker : checkers) { 
-
             ArrayList<Cord> possibleMoves = getPossibleMoves(checker); 
-
             for (Cord move : possibleMoves) { 
-
                 moves.add(new Move(checker, move, false)); 
-
             } 
-
         } 
-        return moves; 
-} 
-
- // Get all possible moves for a single checker 
-
-    private ArrayList<Cord> getPossibleMoves(Checker piece) { 
-
-        ArrayList<Cord> moves = new ArrayList<>(); 
-
-        // If board is null, return empty list 
-
-        if (board == null) { 
-
-            return moves; 
-
-        } 
-
-        int x = piece.getCord().getX(); 
-
-        int y = piece.getCord().getY(); 
-
-        // For kings, check both forward and backward moves 
-
-        // if (piece.isKing()) { 
-
-        //     // Forward moves 
-
-        //     if (board.moveForwardCheck(piece, new Cord(x + 1, y + 1))) { 
-
-        //         moves.add(new Cord(x + 1, y + 1)); 
-
-        //     } 
-
-        //     if (board.moveForwardCheck(piece, new Cord(x - 1, y + 1))) { 
-
-        //         moves.add(new Cord(x - 1, y + 1)); 
-
-        //     } 
-
-        //     // Backward moves 
-
-        //     if (board.moveBackwardCheck(piece, new Cord(x + 1, y - 1))) { 
-
-        //         moves.add(new Cord(x + 1, y - 1)); 
-
-        //     } 
-
-        //     if (board.moveBackwardCheck(piece, new Cord(x - 1, y - 1))) { 
-
-        //         moves.add(new Cord(x - 1, y - 1)); 
-
-        //     } 
-
-        // }  
-
-        // For regular pieces, only check moves in their direction 
-
-        // else { 
-
-        //     if (piece.getColor() == Color.BLACK) { 
-
-        //         if (board.moveForwardCheck(piece, new Cord(x + 1, y + 1))) { 
-
-        //             moves.add(new Cord(x + 1, y + 1)); 
-
-        //         } 
-
-        //         if (board.moveForwardCheck(piece, new Cord(x - 1, y + 1))) { 
-
-        //             moves.add(new Cord(x - 1, y + 1)); 
-
-        //         } 
-
-        //     } else { 
-
-        //         if (board.moveBackwardCheck(piece, new Cord(x + 1, y - 1))) { 
-
-        //             moves.add(new Cord(x + 1, y - 1)); 
-
-        //         } 
-
-        //         if (board.moveBackwardCheck(piece, new Cord(x - 1, y - 1))) { 
-
-        //             moves.add(new Cord(x - 1, y - 1)); 
-
-        //         } 
-
-        //     } 
-
-        // } 
-
         return moves; 
     } 
 
-     */
-    /*
+ 
+    private ArrayList<Cord> getPossibleMoves(Checker piece) { 
+        ArrayList<Cord> moves = new ArrayList<>(); 
+
+        if (board == null) { 
+            return moves; 
+        } 
+
+        int x = piece.getCord().getX(); 
+        int y = piece.getCord().getY(); 
+
+        if (piece.isKing()) { 
+            if (board.moveForwardCheck(piece, new Cord(x + 1, y + 1))) { 
+                moves.add(new Cord(x + 1, y + 1)); 
+            } 
+
+            if (board.moveForwardCheck(piece, new Cord(x - 1, y + 1))) { 
+                moves.add(new Cord(x - 1, y + 1)); 
+            } 
+
+            
+
+            if (board.moveBackwardCheck(piece, new Cord(x + 1, y - 1))) { 
+                moves.add(new Cord(x + 1, y - 1)); 
+            } 
+
+            if (board.moveBackwardCheck(piece, new Cord(x - 1, y - 1))) { 
+                moves.add(new Cord(x - 1, y - 1)); 
+            } 
+
+        }  
+
+       
+        else { 
+
+            if (piece.getColor() == Color.BLACK) { 
+                if (board.moveForwardCheck(piece, new Cord(x + 1, y + 1))) { 
+                    moves.add(new Cord(x + 1, y + 1)); 
+                }
+
+                if (board.moveForwardCheck(piece, new Cord(x - 1, y + 1))) { 
+                    moves.add(new Cord(x - 1, y + 1)); 
+                } 
+
+            } else { 
+
+                if (board.moveBackwardCheck(piece, new Cord(x + 1, y - 1))) { 
+                    moves.add(new Cord(x + 1, y - 1)); 
+                } 
+
+                if (board.moveBackwardCheck(piece, new Cord(x - 1, y - 1))) { 
+                    moves.add(new Cord(x - 1, y - 1)); 
+                } 
+            } 
+        } 
+        return moves; 
+    } 
+
   
-        // select the best jump move based on strategy
+        
     private Move selectBestJumpMove(ArrayList<Move> jumpMoves) {
-        // if there's only one jump move, return it
         if (jumpMoves.size() == 1) {
             return jumpMoves.get(0);
         }
         
-        // prioritize jumps that lead to kings
         ArrayList<Move> kingJumps = new ArrayList<>();
         for (Move move : jumpMoves) {
             if (wouldBecomeKing(move)) {
                 kingJumps.add(move);
             }
         }
-        
+
         if (!kingJumps.isEmpty()) {
             return kingJumps.get(random.nextInt(kingJumps.size()));
         }
         
-        // prioritize jumps that capture kings
         ArrayList<Move> kingCaptureJumps = new ArrayList<>();
         for (Move move : jumpMoves) {
             if (capturesKing(move)) {
                 kingCaptureJumps.add(move);
             }
         }
-        
+
         if (!kingCaptureJumps.isEmpty()) {
             return kingCaptureJumps.get(random.nextInt(kingCaptureJumps.size()));
         }
         
-        // otherwise, choose a random jump move
         return jumpMoves.get(random.nextInt(jumpMoves.size()));
-    } 
+    }
  
-    // select the best move based on strategy
+    
     private Move selectBestMove(ArrayList<Move> moves) {
-        // if there's only one move, return it
         if (moves.size() == 1) {
             return moves.get(0);
         }
         
-        // prioritize moves that lead to kings
         ArrayList<Move> kingMoves = new ArrayList<>();
         for (Move move : moves) {
             if (wouldBecomeKing(move)) {
@@ -309,7 +253,6 @@ public class BotI extends Bot {
             return kingMoves.get(random.nextInt(kingMoves.size()));
         }
         
-        // prioritize moves that protect pieces
         ArrayList<Move> safeMoves = new ArrayList<>();
         for (Move move : moves) {
             if (isSafeMove(move)) {
@@ -321,7 +264,6 @@ public class BotI extends Bot {
             return safeMoves.get(random.nextInt(safeMoves.size()));
         }
         
-        // prioritize moves that advance pieces toward the opponent's side
         ArrayList<Move> advancingMoves = new ArrayList<>();
         for (Move move : moves) {
             if (isAdvancingMove(move)) {
@@ -332,42 +274,34 @@ public class BotI extends Bot {
         if (!advancingMoves.isEmpty()) {
             return advancingMoves.get(random.nextInt(advancingMoves.size()));
         }
-        
-        // otherwise, choose a random move
         return moves.get(random.nextInt(moves.size()));
-    }*/
+    }
  
-    // check if a move would result in the piece becoming a king
-
-    /*
+    
     private boolean wouldBecomeKing(Move move) {
         if (move.piece.isKing()) {
             return false;
         }
-        
         if (move.piece.getColor() == Color.BLACK && move.destination.getY() == 7) {
             return true;
         }
-        
         if (move.piece.getColor() == Color.RED && move.destination.getY() == 0) {
             return true;
         }
-        
         return false;
     }
 
 
     private boolean capturesKing(Move move) 
-    {
-     // If board is null, return false 
-    if (board == null) 
     { 
-     return false; 
-    }  
-    int capturedX = (move.piece.getCord().getX() + move.destination.getX()) / 2; 
-    int capturedY = (move.piece.getCord().getY() + move.destination.getY()) / 2; 
-    Checker capturedPiece = board.checkSpace(new Cord(capturedX, capturedY)); 
-    return capturedPiece != null && capturedPiece.isKing(); 
+        if (board == null) 
+        { 
+        return false; 
+        }  
+        int capturedX = (move.piece.getCord().getX() + move.destination.getX()) / 2; 
+        int capturedY = (move.piece.getCord().getY() + move.destination.getY()) / 2; 
+        Checker capturedPiece = board.checkSpace(new Cord(capturedX, capturedY)); 
+        return capturedPiece != null && capturedPiece.isKing(); 
     } 
 
     private boolean isSafeMove(Move move) 
@@ -384,56 +318,56 @@ public class BotI extends Bot {
                 int y = move.destination.getY() + dy; 
                 if (x >= 0 && x < 8 && y >= 0 && y < 8)
                 { 
-                Checker adjacentPiece = board.checkSpace(new Cord(x, y)); 
-                if (adjacentPiece != null && adjacentPiece.getColor() != color)
-                { 
-                if (adjacentPiece.isKing() ||  
-                (adjacentPiece.getColor() == Color.BLACK && y < move.destination.getY()) || 
-                (adjacentPiece.getColor() == Color.RED && y > move.destination.getY())) 
-                { 
-                    int behindX = move.destination.getX() - dx; 
-                    int behindY = move.destination.getY() - dy; 
-                    if (behindX >= 0 && behindX < 8 && behindY >= 0 && behindY < 8)
+                    Checker adjacentPiece = board.checkSpace(new Cord(x, y)); 
+                    if (adjacentPiece != null && adjacentPiece.getColor() != color)
                     { 
-                        Checker behindPiece = board.checkSpace(new Cord(behindX, behindY)); 
-                        if (behindPiece != null && behindPiece.getColor() == color) 
+                        if (adjacentPiece.isKing() ||  
+                        (adjacentPiece.getColor() == Color.BLACK && y < move.destination.getY()) || 
+                        (adjacentPiece.getColor() == Color.RED && y > move.destination.getY())) 
                         { 
-                            return true; 
+                            int behindX = move.destination.getX() - dx; 
+                            int behindY = move.destination.getY() - dy; 
+                            if (behindX >= 0 && behindX < 8 && behindY >= 0 && behindY < 8)
+                            { 
+                                Checker behindPiece = board.checkSpace(new Cord(behindX, behindY)); 
+                                if (behindPiece != null && behindPiece.getColor() == color) 
+                                { 
+                                    return true; 
+                                } 
+                            } 
+                            return false; 
                         } 
                     } 
-                    return false; 
-                } 
-                } 
                 } 
             } 
         } 
-            return true; 
-        }  
-private boolean isAdvancingMove(Move move)
-{ 
-    if (move.piece.isKing()) 
+        return true; 
+    }
+          
+    private boolean isAdvancingMove(Move move)
     { 
-        return false; // Kings can move in any direction 
-    } 
-    if (move.piece.getColor() == Color.BLACK)
-    { 
-        return move.destination.getY() > move.piece.getCord().getY(); 
-    } 
-    else
-    { 
-        return move.destination.getY() < move.piece.getCord().getY(); 
-    } 
-}  
+        if (move.piece.isKing()) {
+            return false; 
+        } 
+        if (move.piece.getColor() == Color.BLACK)  {
+            return move.destination.getY() > move.piece.getCord().getY(); 
+        } 
+        else { 
+            return move.destination.getY() < move.piece.getCord().getY(); 
+        } 
+    }  
 
-private void executeMove(Move move)
-{ 
-    if (board != null)
+
+    private void executeMove(Move move)
     { 
-        board.updatePosition(move.piece, move.destination); 
-        board.kingMe(move.piece); 
+        if (board != null)
+        { 
+            board.updatePosition(move.piece, move.destination); 
+            board.kingMe(move.piece); 
+        } 
     } 
-    } 
-    private class Move 
+
+    private class Move
     { 
         Checker piece; 
         Cord destination; 
@@ -444,24 +378,28 @@ private void executeMove(Move move)
             this.destination = destination; 
             this.isJump = isJump; 
         } 
-    }  */
-} 
-// private void executeMove(Move move)
-// { 
-//     if (board != null)
-//     { 
-//         board.updatePosition(move.piece, move.destination); 
-//         board.kingMe(move.piece); 
-//     } 
-// } 
-// private class void Move{ 
-//     Checker piece; 
-//     Cord destination; 
-//     boolean isJump; 
-//     Move(Checker piece, Cord destination, boolean isJump)
-//     { 
-//         this.piece = piece; 
-//         this.destination = destination; 
-//         this.isJump = isJump; 
-//     } 
-// } 
+    } 
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
