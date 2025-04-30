943220b39206f199ddc498212ed834394c6fdeaf
dc20258530 <dominikc24@icloud.com>
2025-04-29 23:42:51 -0500

    updated time

--- checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
@@ -470,7 +470,7 @@ public class BotII extends Bot {
         GameMove move = finalMove(gp);
 
         try {
-            Thread.sleep(1000); // Sleep 100 milliseconds to break the infinite recursion
+            Thread.sleep(750); 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
